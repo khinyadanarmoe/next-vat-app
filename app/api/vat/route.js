d6d30f8a92093049f034a5f93fc9e6ac3ced2081
khinyadanarmoe <khinyadanamoe01@gmail.com>
2025-08-20 18:24:08 +0700

    APIs added

--- app/api/vat/route.js
+++ app/api/vat/route.js
@@ -1,7 +1,9 @@
+
 export function GET(request) {
   
-    const rate = 0.7; // Example VAT rate
-    const amount = parseFloat(request.url.searchParams.get('amount')) || 0;
+    const {searchParams} = new URL(request.url);
+    const rate = 0.07; // Example VAT rate
+    const amount = parseFloat(searchParams.get("amount"));
     const vatAmount = amount * rate;
     const totalAmount = amount + vatAmount; 
     return Response.json({ 
